97add887d8794f462ef2ba08e038c8291e08e6fc
Marco Antonio Velasco Torres <145826912+Mulishpudden7u7@users.noreply.github.com>
2024-02-14 10:30:30 -0600

    Add files via upload

---
+++ ejercicio de Mtriz 1.py
@@ -0,0 +1,41 @@
+def main():
+
+    nombres_alumnos = []
+    nombres_materias = []
+
+    for i in range(100):
+        nombre_alumno = input("Ingrese el nombre del alumno {}: ".format(i + 1))
+        nombres_alumnos.append(nombre_alumno)
+
+    for i in range(6):
+        nombre_materia = input("Ingrese el nombre de la materia {}: ".format(i + 1))
+        nombres_materias.append(nombre_materia)
+
+    calificaciones = []
+    for alumno in range(100):
+        calificaciones_alumno = []
+        for materia in range(6):
+            calificacion = float(input("Calificación de {} para {}: ".format(nombres_materias[materia], nombres_alumnos[alumno])))
+            calificaciones_alumno.append(calificacion)
+        calificaciones.append(calificaciones_alumno)
+
+    while True:
+        alumno = int(input("Ingrese el número de alumno (1-100) o -1 para salir: "))
+        if alumno == -1:
+            print("¡Hasta luego!")
+            break
+        if alumno < 1 or alumno > 100:
+            print("Número de alumno inválido.")
+            continue
+        materia = int(input("Ingrese el número de materia (1-6) o -1 para salir: "))
+        if materia == -1:
+            print("¡Hasta luego!")
+            break
+        if materia < 1 or materia > 6:
+            print("Número de materia inválido.")
+            continue
+        calificacion = calificaciones[alumno - 1][materia - 1]
+        print("La calificación del alumno {} en la materia {} es: {}".format(nombres_alumnos[alumno - 1], nombres_materias[materia - 1], calificacion))
+
+if __name__ == "__main__":
+    main()
